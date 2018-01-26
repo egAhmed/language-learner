05c82ea913d9ae0c0bd5ad1715ff5fc99a7d03b3
Bikram Thapa <h2o@h2o-2.local>
2018-01-27 01:37:37 +0200

    New pages added

--- MagarVasa/src/app/app.module.ts
+++ MagarVasa/src/app/app.module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
+import {TabsPage} from '../pages/tabs/tabs';
+import { BasicGreetingsPage } from '../pages/basic-greetings/Basic-greetings';
+import { BasicDialougesPage } from '../pages/basic-dialouges/basic-dialouges';
+import { AdvancedDialougesPage } from '../pages/advanced-dialouges/advanced-dialouges';
+import { ReadWriteLippiPage } from '../pages/read-write-lippi/read-write-lippi';
+import { TestSkillsPage } from '../pages/test-skills/test-skills';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -14,10 +17,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BasicGreetingsPage,
+    BasicDialougesPage,
+    AdvancedDialougesPage,
+    ReadWriteLippiPage,
+    TestSkillsPage
   ],
   imports: [
     BrowserModule,
@@ -26,10 +32,13 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
     HomePage,
-    TabsPage
+    TabsPage,
+    BasicGreetingsPage,
+    BasicDialougesPage,
+    AdvancedDialougesPage,
+    ReadWriteLippiPage,
+    TestSkillsPage
   ],
   providers: [
     StatusBar,
--- MagarVasa/src/pages/home/home.ts
+++ MagarVasa/src/pages/home/home.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BasicGreetingsPage } from '../basic-greetings/Basic-greetings';
+import { BasicDialougesPage } from '../basic-dialouges/basic-dialouges';
+import { AdvancedDialougesPage } from '../advanced-dialouges/advanced-dialouges';
+import { ReadWriteLippiPage } from '../read-write-lippi/read-write-lippi';
+import { TestSkillsPage } from '../test-skills/test-skills';
+
 
 @Component({
   selector: 'page-home',
@@ -9,6 +15,31 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  loadSkillsTestPage(){
+    this.navCtrl.push(TestSkillsPage,{
+        parameters: "check"
+    });
+  }
+  loadBasicGreetingsPage(){
+    this.navCtrl.push(BasicGreetingsPage,{
+        parameters: "check"
+    });
+  }
+  loadBasicDialougesPage(){
+    this.navCtrl.push(BasicDialougesPage,{
+        parameters: "check"
+    });
+  }
+  loadAdvancedDialougesPage(){
+    this.navCtrl.push(AdvancedDialougesPage,{
+        parameters: "check"
+    });
+  }
+  loadReadWriteLippiPage(){
+    this.navCtrl.push(ReadWriteLippiPage,{
+        parameters: "check"
+    });
   }
 
 }
